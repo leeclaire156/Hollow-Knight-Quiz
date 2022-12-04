af18dea534068aaf5bef1d5e14a5b9589b51ffbb
leeclaire156 <lee.claire156@gmail.com>
2022-12-04 18:12:55 -0500

    Allows user to select answers

--- Develop/Assets/script.js
+++ Develop/Assets/script.js
@@ -3,15 +3,19 @@
 var startPage = document.querySelector(".start-pg");
 var startBtn = document.querySelector("#start-btn");
 var scoreBoardBtn = document.querySelector(".scores-btn");
+var playerScore = 0;
 
 //Clicking start leads you to instructions page
 startBtn.addEventListener("click", function () {
     startPage.style.display = "none";
-    // instructionPage.style.display = "block";
+    // instructionPage.style.display = "block";\
+
+
     //!!!!!!!!!!!!TODO DELETE BELOW AND UNCOMMENT ABOVE
     quizPage.style.display = "grid";
     startTimer();
     loadQuestion(0);
+    playerScore = 0;
 });
 
 //Clicking start leads you to score board/high score page
@@ -31,6 +35,7 @@ continueBtn.addEventListener("click", function () {
     //TODO uncomment below!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     // startTimer();
     // loadQuestion(0);
+    //playerScore = 0;
 });
 
 //Quiz Page
@@ -141,16 +146,7 @@ var questionsBank = [
 
 var questionNumb = 0;
 
-//Next button clicking cycles through questions
-nextBtn.addEventListener("click", function () {
-    if (questionNumb < questionsBank.length - 1) {
-        questionNumb++;
-        loadQuestion(questionNumb);
-    } else {
-        console.log("doe");
-    }
 
-});
 
 //This is how we change the questions based on the index
 function loadQuestion(index) {
@@ -159,13 +155,79 @@ function loadQuestion(index) {
     choice2.innerText = questionsBank[index].choices[1];
     choice3.innerText = questionsBank[index].choices[2];
     choice4.innerText = questionsBank[index].choices[3];
-
-
-
-    // for (var i = 0; i <= choice.length; i++) {
-    //     choice[i].setAttribute("onclick", "optionSelected(this)");
-    // }
+    //Disables the next button upon loading
+    nextBtn.classList.add("disabled")
 }
+
+
+allChoices.forEach((choices, playerAns) => {
+    choices.addEventListener("click", () => {
+        //Enables the next button upon choosing an answer
+        nextBtn.classList.remove("disabled")
+
+        if (playerAns == questionsBank[questionNumb].correct) {
+            playerScore += 100;
+            console.log(playerScore);
+            playerAns.style("color:green;")
+        } else {
+            //If the player clicks on the wrong answer, 5 seconds are taken off the clock
+            secondsLeft -= 5;
+        }
+
+        //To be deleted - highlights answer chosen in grey
+        choices.classList.add("active");
+
+        //disable option choices When player selects an answer
+        for (i = 0; i <= 3; i++) {
+            allChoices[i].classList.add("disabled");
+        }
+
+    })
+
+});
+
+
+
+//Next button clicking cycles through questions
+nextBtn.addEventListener("click", function () {
+    //Re-enables option choices upon loading next question
+    for (i = 0; i <= 3; i++) {
+        allChoices[i].classList.remove("disabled");
+    }
+
+    //To be deleted - removes highlight from last question's choices
+    allChoices.forEach((choices) => {
+        choices.classList.remove("active");
+    });
+
+    //Loads next question
+    if (questionNumb < questionsBank.length - 1) {
+        questionNumb++;
+        loadQuestion(questionNumb);
+    } else {
+        //save score function here
+        quizPage.style.display = "none";
+        saveScorePage.style.display = "block";
+        // clearInterval(timer);
+        // secondsLeft = 5 * 60;
+    }
+});
+
+
+
+
+//Score Keeper on Quiz Page
+//SCORESESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+var playerScoreTracker = document.querySelector(".player-score-tracker");
+
+
+
+
+
+
+
+
+
 
 
 
@@ -185,18 +247,36 @@ function startTimer() {
         timerCountdown.innerHTML = `${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`;
         if (secondsLeft == 0 || secondsLeft < 1) {
             clearInterval(timer);
-            endTime();
+            ranOuttaTime();
         }
     }, 1000);
 }
 
-function endTime() {
+function ranOuttaTime() {
     //!!TODO replace below with function based on quiz results
     timerCountdown.innerHTML = "NONE";
+    quizPage.style.display = "none";
+
 }
 
-//Score Keeper on Quiz Page
-var playerScore = document.querySelector(".player-score");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Save Score Page
